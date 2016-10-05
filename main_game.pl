dc78cca488706b40813435ab581c73f445fa0657
makrau <iago006@hotmail.com>
2016-10-04 23:56:47 -0300

    Implemented the print_bag primitive and the taken an item action.

--- main_game.pl
+++ main_game.pl
@@ -31,10 +31,10 @@ objects(key2).
 objects(knife).
 
 start :- write('Welcome to <game_name>'), nl,
-   write("it's an alpha version, so it's still not complete"), nl, nl,
+   write("it's an alpha version, so it's still not complete"), nl,
    play.
 
-play :- write('You are currently in the '),
+play :- nl, write('You are currently in the '),
    current_location(Current_location),
    write(Current_location), nl,
    write('and you can: '), nl,
@@ -48,9 +48,19 @@ print_options :- current_location(Current_location), options_per_location(Curren
 
 print_options :- current_location(Current_location), options_per_location(Current_location, Option), objects(Option),
    tab(4), write('Grab => '), write(Option), nl, fail.
+print_options :- tab(4), write('See itens on bag => bag').
 
 print_options.
 
 handle_option(Choosen_option) :- locations(Choosen_option), !, retract(current_location(X)), assert(current_location(Choosen_option)).
-handle_option(Choosen_option) :- objects(Choosen_option), !, current_location(X), retract(options_per_location(X, Choosen_option)).
-% handle_option(Choosen_option) :- objects(Choosen_option), !, assert(bag(Choosen_option)).
+handle_option(Choosen_option) :- objects(Choosen_option), !, current_location(X), retract(options_per_location(X, Choosen_option)), assert(bag(Choosen_option)).
+handle_option(bag) :- print_bag.
+
+print_bag :- bag(X), !, nl, write('The currents items on your bag are: '), print_item_bag.
+print_bag :- not(bag(X)), print_empty_bag.
+print_bag.
+
+print_item_bag :- nl, bag(Item), tab(4), write('* '), write(Item), nl, fail.
+print_item_bag.
+
+print_empty_bag :- nl, write('Your bag is currently empty.'), nl.
